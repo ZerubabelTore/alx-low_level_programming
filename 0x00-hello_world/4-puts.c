3d8e0cb37f278a65690f2b0e8913103be1c68355
zerubabeltore <zerishtore@gmail.com>
2022-04-07 12:36:55 -0700

    Task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - the entry point for the program
+ *main - the entry point for the program
  *
- * Returen: 0 is the exit after
+ *Returen: 0 is the exit after
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
