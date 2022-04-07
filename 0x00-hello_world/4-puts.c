2061027172287d7ff1200d97511db947613a20a6
zerubabeltore <zerishtore@gmail.com>
2022-04-07 13:20:17 -0700

    Task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,73 +1,10 @@
-R: trailing whitespace
-total: 1 errors, 0 warnings, 11 lines checked
-main
-root@46afd0270ce6:/alx-low_level_programming/0x00-hello_world# vi 4-puts.c
-root@46afd0270ce6:/alx-low_level_programming/0x00-hello_world# betty 4-puts.c
-
-========== 4-puts.c ==========
-4-puts.c:3: ERROR: trail 
-
-q
-'
-`
-`
-[A[A[B
-
-
-
-
-
-q
-
-
-
-
-clear
-qwertyuiopasdfghjklzxcvbnm
-
-[3~
-
-
-
-
-
-
-
-
-exit
-q;q
-:q
-
-w
-:q
-
-clear
-clear
-
-g
-
-q
-Q
-w
-W
-vi
-
-;'
-'
-"
-/
-;?
-//
-\
-|
-||
-}
-]
-
-clear
-
-
-
-
-
-t
+#include <stdio.h>
+  2 /**
+  3  *main - entry point
+  4  *Return: 0 after printing the function
+  5  */
+  6 int main(void)
+  7 {
+  8 puts("\"Programming is like building a multilingual puzzle");
+  9 return (0);
+ 10 }
