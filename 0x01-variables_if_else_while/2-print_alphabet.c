146bea7b48d1438f5f3812a4fb01a1b21b1d2345
zerubabeltore <zerishtore@gmail.com>
2022-04-08 10:35:45 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main 
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char A = a;
+
+	while (A <= z)
+	{
+		putchar(A);
+		A++;
+
+		putchar('\n');
+		return (0);
+	}
+}
