91f140c8d91faa71929c94317b6d3c6e7d94762e
zerubabeltore <zerishtore@gmail.com>
2022-04-08 11:40:41 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  * main - this code will print the 10 digit numbers
+  *
+  * Return: Always (Success)
+  */
+int main(void)
+{
+	int a = 0;
+	while (a <= 9)
+	{
+	    printf("%d", a);
+	    a++;
+	}
+	return (0);
+
+}
