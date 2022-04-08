7effa2a2021e86053dcc8130f74a5f2cf5abda01
zerubabeltore <zerishtore@gmail.com>
2022-04-08 12:07:21 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+  * main - this code will print numbers from 0 to 9 and alphabets
+  * from a to f in lowercase
+  *
+  * Return: Always (Success)
+  */
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
+
