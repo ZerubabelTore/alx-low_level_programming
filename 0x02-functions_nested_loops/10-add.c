0f63675c5c27b22b8425f26b5d1b793a96831c07
zerubabeltore <zerishtore@gmail.com>
2022-04-12 11:52:00 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - this function will print th addition
+ * of two numbers
+ * @c: print int c
+ * @d: print int d
+ * Return: the addition of c and d.
+ */
+
+int add(int c, int d)
+{
+	return (c + d);
+}
