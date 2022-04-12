151e87763f2b1b5dff3e5991c9d2f3d19ddf5ee1
zerubabeltore <zerishtore@gmail.com>
2022-04-12 14:20:53 -0700

    advanced task 4

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,7 +2,7 @@
 
 /**
  * main - this function withis function will finds and
- * 	prints the first 98 Fibonacci numbers,
+ *	prints the first 98 Fibonacci numbers,
  * starting with 1 and 2
  * followed by a new line
  * Return: ALways 0 (Success)
