3dda4e3a48578749be4967a8816b9c9136b0e828
zerubabeltore <zerishtore@gmail.com>
2022-05-19 23:50:40 -0700

    Advanced task 2

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,26 @@
+; File: 101-hello_holberton.asm
+; Auth: Zerubabel Tore
+; Desc: this is 64-bit assembly program that prints
+;       "Hello, Holberton" followed by a new line.
+
+extern print
+
+section .text
+   global main
+
+main:
+   push rbp
+
+   mov rdi,fmt
+   mov rsi,msg
+   mov rax,0
+   call printf
+
+   pop rbp
+
+   mov rax,0
+   ret
+
+section .data
+   msg: db "Hello, Holberton", 0
+   fmt: db "%s", 10, 0
