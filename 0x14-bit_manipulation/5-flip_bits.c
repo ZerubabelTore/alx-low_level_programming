d9469862701f1d144f2a90687f6163ee3ec950ef
zerubabeltore <zerishtore@gmail.com>
2022-05-26 15:29:50 -0700

    Task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * flip_bits - this function will write a function that returns
+ * the number of bits you would
+ * need to flip to get from one number to another.
+ *
+ * @n: is a umber Input 1.
+ * @m: is a number Input 2.
+ *
+ * Return: Number of flip in the Input numbers.
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+
+{
+	unsigned int flip = 0, comp, comp1;
+
+	while (!(n == 0 && m == 0))
+	{
+		comp = n & 1;
+		comp1 = m & 1;
+		n = n >> 1;
+		m = m >> 1;
+		if (comp != comp1)
+			flip += 1;
+	}
+	return (flip);
+}
