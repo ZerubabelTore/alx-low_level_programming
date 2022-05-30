02199b8e0d52c8e3d2835d43e59344a51ce622a6
zerubabeltore <zerishtore@gmail.com>
2022-05-30 13:26:32 -0700

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
 	int file_from, file_to;
 	int num1 = 1024, num2 = 0;
 	char buf[1024];
-	
+
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	file_from = open(argv[1], O_RDONLY);
@@ -31,7 +31,7 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		close(file_from), exit(99);
 	}
-	
+
 	while (num1 == 1024)
 	{
 		num1 = read(file_from, buf, 1024);
@@ -44,12 +44,10 @@ int main(int argc, char *argv[])
 		if (num2 < num1)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
-	
 	if (close(file_from) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
 	if (close(file_to) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
-	
 	return (0);
 
 }
